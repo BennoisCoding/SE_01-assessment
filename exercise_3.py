b5c05e10392c3d49c33a94d0c668b55140ff2927
Benno Klumker <benno.klumker@code.berlin>
2023-12-06 17:55:32 +0100

    Task_3

--- exercise_3.py
+++ exercise_3.py
@@ -6,6 +6,7 @@ def special_bomb(list_1, list_2):
     for cell in list_1:
         if cell in list_2 and list_1[1] != list_2[1]:
             print("The move was a special bomb.")
+            quit()
 
 
 def legal_move(field):
@@ -72,11 +73,86 @@ def legal_move(field):
         special_bomb(special_chk_list_row, special_chk_list_column)
     else:
         quit()
+    
+    # If no matching colors were found the move was not legal
+    print("The move is not legal.")
+    return False
 
 
-field = [[1, 1, 2, 3],
-         [2, 2, 2, 3], 
-         [3, 1, 2, 1],
-         [3, 7, 6, 1]]
+def candy_crush(field):
+    """Let the player make a move and determine if it is a legal move."""
 
-legal_move(field)
+    # Printing initial game_field
+    print("\n")
+    for row in field:
+        print(row)
+    print("\n")
+
+    x_start = 0
+    y_start = 0
+
+    # Choose the diamond's coordinates you want to move. 
+    while x_start is 0 or y_start is 0:
+        y_start = int(input("Pls enter the row coordinate of the diamond you want to move (1-4): "))
+        x_start = int(input("Pls enter the column coordinate of the diamond you want to move (1-4): "))
+
+        # Check that the coordindates are valid.
+        if x_start not in range(1, 5) or y_start not in range(1, 5):
+            x_start = 0
+            y_start = 0
+            print("You did not choose valid coordinates. Pls try again.")
+
+    direction = ""
+    x_end = 0
+    y_end = 0
+
+    # Choose the direction in which you want to move the diamond. 
+    while x_end is 0 or y_end is 0:
+        print("\nPls enter the direction where you want to move the diamond.")
+        print("You can only move up, down, left or right and not outside of the game field.\n")
+        direction = input("Write 'UP' or 'DOWN' for moving up or down and 'LEFT' or 'RIGHT' for moving left or right: ").upper()
+
+        if direction == "UP":
+            y_end += y_start - 1
+            x_end = x_start
+        elif direction == "DOWN":
+            y_end += y_start + 1
+            x_end = x_start
+        elif direction == "LEFT":
+            y_end = y_start
+            x_end += x_start - 1
+        elif direction == "RIGHT":
+            y_end = y_start
+            x_end += x_start + 1
+
+        # Check that the direction is valid
+        if x_end not in range(1, 5) or y_end not in range(1, 5):
+            x_end = 0
+            y_end = 0
+            print("You did not choose a valid direction. Pls try again.")   
+
+    # Switching positions of the diamonds
+    first_diamond = None
+    second_diamond = None
+
+    first_diamond = field[y_start - 1][x_start - 1]
+    second_diamond = field[y_end - 1][x_end - 1]
+
+    field[y_end - 1][x_end - 1] = first_diamond
+    field[y_start - 1][x_start - 1] = second_diamond
+
+    legal_move(field)
+
+    # Printing game_field after move
+    for row in field:
+        print(row)
+    print("\n")
+
+
+game_field = [[2, 1, 4, 3],
+              [4, 4, 4, 5], 
+              [2, 5, 4, 4],
+              [6, 7, 6, 1]]
+
+
+candy_crush(game_field)
